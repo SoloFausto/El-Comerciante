3aaaff69c89a8b7da9d1e914bc42fd8c081d7a79
faustorm07 <faustor318@gmail.com>
2022-06-18 08:43:35 -0300

    android 1st release

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.*;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        // Binding MainActivity.java with
+        // activity_main.xml file
+        setContentView(R.layout.activity_main);
+
+        // Find the WebView by its unique ID
+        WebView w = (WebView) findViewById(R.id.web);
+
+        // loading http://www.google.com url in the the WebView.
+        //w.loadUrl("file:///android_asset/tablausuarios.html");
+        w.loadUrl("https://www.facebook.com");
+        // this will enable the javascript.
+        w.getSettings().setJavaScriptEnabled(true);
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        w.setWebViewClient(new WebViewClient());
+
+    }
+}
