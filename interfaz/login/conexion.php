a378010b8a166e2ed047a8463d657ba51c7a33f7
Fausto <faustor318@gmail.com>
2022-08-10 09:27:01 -0300

    fe

---
+++ interfaz/login/conexion.php
@@ -0,0 +1,12 @@
+<?php
+    function conectar(){
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "login_dolcezza";
+
+        $con = mysqli_connect($host, $user, $pass);
+        mysqli_select_db($con, $db);
+        return $con;
+    }
+?>
