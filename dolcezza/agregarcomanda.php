cf4dc7b4a80ad754801856218d1f35564305a6db
Estenux <emiliotagliani81@gmail.com>
2022-08-10 08:41:31 -0300

    login+interfaz

---
+++ dolcezza/agregarcomanda.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <title>Agregar Comanda</title>
+</head>
+<body>
+     <h1 class="addcom">Agregar Comanda</h1>
+    <h3 class="pro">Productos a pedir:</h3>
+    
+    <a class="close" href="inicio.php">
+    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="auto" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+</a>
+    
+    <table class="mesas">
+    <tr>
+            <td>Mesas:</td>
+    </tr>
+    </table>
+    <table class="llevar"> 
+    <tr>
+            <td>Llevar:</tr>
+    </tr>
+    </table>
+
+    <div class="add">
+<a href="agregarcomanda.php">
+<svg xmlns="http://www.w3.org/2000/svg" width="40" height="auto" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg>
+</div>
+</a> 
+
+    <table class="first">
+        <tr>
+            <td>(Nombre del envase)</td>
+            <td>Cantidad de sabores:</td>
+            <td>Precio:</td>
+            <td><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="auto" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+</svg></a></td>
+        </tr>
+    </table>
+    <table class="tastes">
+        <tr>
+            <td>Sabor1</td>
+            <td class="log"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="auto" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg></a></td>
+        </tr>
+    </table>
+
+    <table class="tastes">
+        <tr>
+            <td>Sabor2</td>
+            <td class="log"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="auto" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg></a></td>
+        </tr>
+    </table>
+
+    <table class="tastes">
+        <tr>
+            <td>Sabor3</td>
+            <td class="log"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="auto" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg></a></td>
+        </tr>
+    </table>
+        
+    <table class="second">
+        <tr>
+            <td>(Nombre del articulo)</td>
+            <td>Cantidad:</td>
+            <td>Precio:</td>
+            <td><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="auto" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+</svg></a></td>
+        </tr>
+    </table>
+    <table class="cash">
+        <tr>
+    <td><h1>Precio total:</h1></td>
+    <td><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="40" height="auto" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+  <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+  <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+</svg></td>
+        </tr>
+    </table>
+    <style>
+        .cash{
+            text-align: left;
+    float:right;
+    border: 1px solid black;
+    width: 550px;
+    height: 50px;
+    float: right;
+    margin-right: 27%;
+    margin-top: 3%;
+        }
+        .second{
+    text-align: left;
+    float:right;
+    border: 1px solid black;
+    width: 550px;
+    height: 50px;
+    float: right;
+    margin-right: 27%;
+    margin-top: 3%;
+        }
+        .log{
+            float: right;
+            margin-right: 10%;
+        }
+        .tastes{
+    text-align: left;
+    float:right;
+    border: 1px solid black;
+    width: 550px;
+    height: 50px;
+    float: right;
+    margin-right: 27%;
+    margin-top: 1%;
+        }
+        .first{
+    text-align: left;
+    float:right;
+    border: 1px solid black;
+    width: 550px;
+    height: 50px;
+    float: right;
+    margin-right: 12%;
+    margin-top: 3%;
+        }
+        .add{
+            float: right;
+            margin-right: 150px;
+        }
+        .close{
+            float: right;
+            margin-right: 150px;
+        }
+        .mesas{
+            border:1px solid black;
+            padding:10px;
+            text-align: center;
+            margin-top:10px;
+            margin-left: 150px;
+        }
+        .llevar{
+            border:1px solid black;
+            padding:10px;
+            text-align: center;
+            margin-top: 10px;
+            margin-left: 150px;
+        }
+        .addcom{
+            text-align: center;
+        }
+        .pro{
+            text-align: center;
+        }
+        </style>
+</body>
+</html>
