5d6151a4bf42b912d251bc20587e85fb17ab211c
faustorm07 <faustor318@gmail.com>
2022-08-20 02:45:35 -0300

    Update estadisticas.php

--- view/dolcezzainterfaces/personal/estadisticas/estadisticas.php
+++ view/dolcezzainterfaces/personal/estadisticas/estadisticas.php
@@ -11,7 +11,7 @@
 
 <body>
 <div class="title">
-    <h1>estadisticas</h1>
+    <h1>Estadisticas</h1>
   </div>
 <div class="page">
 <div class="content">
@@ -24,79 +24,108 @@
   <div class="icon">
     <a href="inicio.php">
     <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="54" fill="black" class="bi bi-house" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="58" fill="black" class="bi bi-house" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
         <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
       </svg>
       
     </div>
+    </a>
   </div>
   
   <div class="icon">
     <a href="../comandas/comanda.php">
-    <div class="notes">
-      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="auto" fill="black" class="bi bi-sticky" viewBox="0 0 16 16">
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="58" fill="black" class="bi bi-sticky" viewBox="0 0 16 16">
         <path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1h-11zM2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V8H9.5A1.5 1.5 0 0 0 8 9.5V14H2.5a.5.5 0 0 1-.5-.5v-11zm7 11.293V9.5a.5.5 0 0 1 .5-.5h4.293L9 13.793z"/>
       </svg>
-   
+    </div>
+    </a>
   </div>
+
   <div class="icon">
+    <div>
     <a href="../dispositivosVinculados/dispositivos.php">
-      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="auto" fill="black" class="bi bi-tablet" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="58" fill="black" class="bi bi-tablet" viewBox="0 0 16 16">
         <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
         <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
       </svg>
+      </div>
+      </a>
   </div>
+
   <div class="icon">
     <a href="../estadisticas/estadisticas.php">
-    <div class="estadisticas1">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="auto" fill="black" class="bi bi-bar-chart-fill" viewBox="0 0 16 16">
+    <div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="58" fill="black" class="bi bi-bar-chart-fill" viewBox="0 0 16 16">
       <path d="M1 11a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3zm5-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V2z"/>
     </svg>
+    </div>
+    </a>
   </div>
+
   <div class="icon">
     <a href="#">
-    <div class="comandas1">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="black" class="bi bi-list-ol" viewBox="0 0 16 16">
+    <div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="58" fill="black" class="bi bi-list-ol" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/>
       <path d="M1.713 11.865v-.474H2c.217 0 .363-.137.363-.317 0-.185-.158-.31-.361-.31-.223 0-.367.152-.373.31h-.59c.016-.467.373-.787.986-.787.588-.002.954.291.957.703a.595.595 0 0 1-.492.594v.033a.615.615 0 0 1 .569.631c.003.533-.502.8-1.051.8-.656 0-1-.37-1.008-.794h.582c.008.178.186.306.422.309.254 0 .424-.145.422-.35-.002-.195-.155-.348-.414-.348h-.3zm-.004-4.699h-.604v-.035c0-.408.295-.844.958-.844.583 0 .96.326.96.756 0 .389-.257.617-.476.848l-.537.572v.03h1.054V9H1.143v-.395l.957-.99c.138-.142.293-.304.293-.508 0-.18-.147-.32-.342-.32a.33.33 0 0 0-.342.338v.041zM2.564 5h-.635V2.924h-.031l-.598.42v-.567l.629-.443h.635V5z"/>
     </svg>
+    </div>
+    </a>
   </div>
+  <div class="empty"></div>
   <div class="icon">
     <a href="#">
-    <div class="settings">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="auto" fill="black" class="bi bi-gear" viewBox="0 0 16 16">
+    <div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="58" fill="black" class="bi bi-gear" viewBox="0 0 16 16">
       <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
       <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
     </svg>
+    </div>
+    </a>
   </div>
   <div class="icon">
     <a href="#">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="auto" fill="black" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+      <div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="58" fill="black" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
       <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
     </svg>
+    </div>
+    </a>
   </div>
 
 </div>
 
 
 <style>
+  .empty{
+    height:50vh;
+    width:50px;
+  }
   .title{
     display: flex;
+    align-items: center;
     border:1px solid black;
+    width: 97.4vw;
+    position: fixed;
+    height: 50px;
+    left: 51px;
+    top: 0px;
   }
   .page{
-    display: flex;
-    flex-direction: row-reverse;
     
   }
   .navbar{
+    position:fixed;
     border:1px solid black;
     width: fit-content;
-    flex:auto;
+    flex: 0 1 0;
     flex-direction: column;
     display: flex;
-
+    bottom: -2px;
+    left: 0px;
+    height: 100vh;
   
   }
   .icon{
@@ -104,17 +133,29 @@
     flex: auto;
     display: flex;
     width:auto;
-    
-
+    filter: invert(0%) sepia(100%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%);
   }
-  .warning{
-    
+  svg:hover{
+    flex-direction: column;
+    flex: auto;
+    display: flex;
+    width:auto;
+    filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(305deg) brightness(101%) contrast(102%);
+    }
+  .icon:hover{
+    background-color: black;
+  }
+  .a_icon{
+    height: 54px;
   }
   .content{
-    border:1px solid black;
     display: flex;
     flex: auto;
     flex-direction: row;
+    position: fixed;
+    left: 51px;
+    width: 97.4vw;
+    top:50px;
     }
     .stat{
       flex:auto;
