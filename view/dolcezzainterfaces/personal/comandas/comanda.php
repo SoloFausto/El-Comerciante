f4b49e0d370443d1f53d81823802bba519be573f
Fausto <faustor318@gmail.com>
2022-08-24 12:07:14 -0300

    dsadsa

--- view/dolcezzainterfaces/personal/comandas/comanda.php
+++ view/dolcezzainterfaces/personal/comandas/comanda.php
@@ -27,18 +27,14 @@
     <div class="table-responsive table-wrapper">
     <table class="table ">
     <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr><tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr><tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
+    <td><div><p>*FOTO*</p></div>
+                        <div><p>*Nombre de producto*</p></div>
+                        <div><p>*Precio*</p></div>
+                        <div><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg></div>
+                    </td>
   </tr>
+
     </table>
     </div>  
   </div>
