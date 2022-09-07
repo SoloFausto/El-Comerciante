45bcad049b8d3de744693923e810cfdc5d77bb08
sebamestoy <emiliotagliani81@gmail.com>
2022-09-07 09:51:04 -0300

    .....
    ..

--- view/dolcezzainterfaces/personal/comandas/desplegables/agregarorden.php
+++ view/dolcezzainterfaces/personal/comandas/desplegables/agregarorden.php
@@ -1,58 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../../estiloPersonal/estiloAgregarOrden.css">
-    <title>Agregar a la orden</title>
-</head>
-<body>
-    
-    <h1 class="title">Agregar a la orden</h1>
-        
-    <a href="agregarcomanda.php">
-    <div class="close">
-    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="auto" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-    </svg>
+<div class="agregarPlatoPopUp">
+        <div class="background" style="z-index: 2;"></div>
+        <div class="menuAdd">
+            <div class="addOrderTitle">
+            <button type="button" onclick="removeAgregarPlato()" style="border:0px;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+              </svg>
+            </button>
+            <h2>Agregar a la orden</h2>
+            <hr>
+        </div>
+        <div>
+            <table class="table">
+                
+                    <tr>
+                        <td colspan="2"><div><h4>Envases</h4></div></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button onclick="agregarHelado()"><div><p>Envase de N kilos (N Sabores)</p></div></button></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button onclick="agregarHelado()"><div><p>Envase de N kilos (N Sabores)</p></div></button></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button onclick="agregarHelado()"><div><p>Envase de N kilos (N Sabores)</p></div></button></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button onclick="agregarHelado()"><div><p>Envase de N kilos (N Sabores)</p></div></button></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><div><h4>Comidas</h4></div></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><a href=""><div><p>(Titulo de comida)</p></div></a></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><a href=""><div><p>(Titulo de comida)</p></div></a></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><a href=""><div><p>(Titulo de comida)</p></div></a></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><a href=""><div><p>(Titulo de comida)</p></div></a></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><a href=""><div><p>(Titulo de comida)</p></div></a></td>
+                    </tr>
+                    <tr>
+                        
+                        <td></td>
+                        <td><a href=""><div><p>(Titulo de comida)</p></div></a></td>
+                    </tr>
+            </table>
+        </div>
+        <br>
+        </div>
     </div>
-    </a>
-    <table class="envases1">
-    <tr>
-    <td><h2>Envases</h2></td>
-    </tr>
-    </table>
-    <table class="envases2">
-    <tr>
-    <td><h2>Envases N Kilos</h2></td>
-    </tr>
-    </table>
-    <table class="envases3">
-    <tr>
-    <td><h2>Envases N Kilos</h2></td>
-    </tr>
-    </table>
-    <table class="envases4">
-    <tr>
-    <td><h2>Envases N Kilos</h2></td>
-    </tr>
-    </table>
-    <table class="envases5">
-    <tr>  
-    <td><h2>Cono de N sabores</h2></td>
-    </tr>
-    </table>
-    <table class="cash">
-    <tr>
-    <td><a href="agregarcomanda.php"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="auto" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
-    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
-    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
-    </svg></td>
-    </tr>
-    </table>
-
-</body>
-</html>
--- view/dolcezzainterfaces/personal/comandas/desplegables/seleccionarsabores.php
+++ view/dolcezzainterfaces/personal/comandas/desplegables/seleccionarsabores.php
@@ -1,55 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="../../estiloPersonal/estiloSeleccionarSabores.css">
-    <title>Seleccionar Sabores</title>
-</head>
-<body>
-    
-    <h1 class="title">Selecciona sabores</h1>
-        
-    <a href="agregarcomanda.php">
-    <div class="close">
-    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="auto" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-    </svg>
+<div class="popupHelado">
+        <div class="background" style="z-index:7;"></div>
+        <div class="menuAdd"style="z-index:8;">
+            <div class="addOrderTitle">
+            <button type="button" onclick="removeAgregarHelado()" style="border:0px;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+              </svg>
+            </button>
+            <h2 id="Title">Selecciona N Sabores</h2>
+            <hr>
+        </div>
+        <div>
+            <form action=""></form>
+            <table class="table">
+                
+                    <tr>
+                        <td colspan="2">
+                            <div><h4>(Nombre de el sabor)</h4></div>
+                            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" onclick="func()">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div><h4>(Nombre de el sabor)</h4></div>
+                            <input type="checkbox" id="vehicle2" name="vehicle2" value="Bike"onclick="func()">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div><h4>(Nombre de el sabor)</h4></div>
+                            <input type="checkbox" id="vehicle3" name="vehicle3" value="Bike"onclick="func()">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div><h4>(Nombre de el sabor)</h4></div>
+                            <input type="checkbox" id="vehicle4" name="vehicle4" value="Bike" onclick="func()">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div><h4>(Nombre de el sabor)</h4></div>
+                            <input type="checkbox" id="vehicle5" name="vehicle5" value="Bike" onclick="func()">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div><h4>(Nombre de el sabor)</h4></div>
+                            <input type="checkbox" id="vehicle6" name="vehicle6" value="Bike" onclick="func()">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <div><h4>(Nombre de el sabor)</h4></div>
+                            <input type="checkbox" id="vehicle7" name="vehicle7" value="Bike" onclick="func()">
+                        </td>
+                    </tr>
+            </table>
+            <div style="text-align: center;">
+                <button type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+                  </svg>
+                </button>
+            </div>
+        </form>
+        </div>
+        <br>
+        </div>
     </div>
-    </a>
-    <table class="sabor1">
-    <tr>
-    <td><h2>(Nombre el sabor)</h2></td>
-    </tr>
-    </table>
-    <table class="sabor2">
-    <tr>
-    <td><h2>(Nombre del sabor)</h2></td>
-    </tr>
-    </table>
-    <table class="sabor3">
-    <tr>
-    <td><h2>(Nombre del sabor)</h2></td>
-    </tr>
-    </table>
-    <table class="sabor4">
-    <tr>
-    <td><h2>(Nombre del sabor)</h2></td>
-    </tr>
-    </table>
-
-    <table class="cash">
-    <tr>
-    <td><a href="agregarcomanda.php"><svg xmlns="http://www.w3.org/2000/svg" width="40" height="auto" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
-    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
-    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
-    </svg></td>
-    </tr>
-    </table>
-
-
-</body>
-</html>
