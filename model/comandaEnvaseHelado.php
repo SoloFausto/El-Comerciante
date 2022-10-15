41b743cef5474c63a2cfffbceef477a2193f2fc9
Fausto <faustor318@gmail.com>
2022-10-15 11:24:52 -0200

    Create comandaEnvaseHelado.php

---
+++ model/comandaEnvaseHelado.php
@@ -0,0 +1,19 @@
+<?php
+// {}
+    require "comanda.php";
+    require "envase.php";
+    require "helado.php";
+class comandaEnvaseHelado {
+    private $idEnvase;
+    private $idHelado;
+    private $numComanda;
+    private $numEnvase;
+    private $cantidad;
+    private $conn;
+    function __construct(){
+        
+    }
+}
+
+
+?>
