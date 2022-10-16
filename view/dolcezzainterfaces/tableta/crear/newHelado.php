06d057b805b1336dc4bcf8991bfa1dbed44904b9
Estenux <emiliotagliani81@gmail.com>
2022-10-16 20:27:34 -0300

    n

--- view/dolcezzainterfaces/tableta/crear/newHelado.php
+++ view/dolcezzainterfaces/tableta/crear/newHelado.php
@@ -13,20 +13,26 @@
 <body>
     <div class="container-xs">
         <!-- Este boton te hace volver-->
+        <form action="../helados/helados.php">
         <div class="container-fluid">
-            <button type="button" class="btn btn-outline-dark">
+            <button type="submit" class="btn btn-outline-dark">
                 <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                 </svg>
             </button>
-        </div>
-        <div class="container text-center">
-            <div class="container">
+            <div class="container ss">
                 <h2>Seleccione los sabores</h2>
             </div>
+        </div>
+        </form>
+        <div class="container text-center">
             <?php include("../includes/productss.php"); ?>
             <div class="container-fluid">
-                <button type="button" class="btn btn-light">Agregar a la orden</button>
+                <button type="button" class="btn btn-light">Agregar a la orden
+                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+                    </svg>
+                </button>
             </div>
         </div>
     </div>
--- view/dolcezzainterfaces/tableta/includes/listaProducto.php
+++ view/dolcezzainterfaces/tableta/includes/listaProducto.php
@@ -1,9 +1,11 @@
 <table class="table">
+
+
                 <tr>
                     <td><div><p><img class="imag" src="../imagenes/descarga.jpg" alt=""></p></div>
                         <div><p>*Nombre de producto*</p></div>
                         <div><p>*Precio*</p></div>
-                        <div><a href="../crear/newProduct.html"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <div><a href="../crear/newProduct.php"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg></a></div>
                     </td>
                 </tr>
@@ -11,7 +13,7 @@
                     <td><div><p><img class="imag" src="../imagenes/descarga.jpg" alt=""></p></div>
                         <div><p>*Nombre de producto*</p></div>
                         <div><p>*Precio*</p></div>
-                        <div><a href="../crear/newProduct.html"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <div><a href="../crear/newProduct.php"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg></a></div>
                     </td>
                 </tr>
@@ -19,15 +21,7 @@
                     <td><div><p><img class="imag" src="../imagenes/descarga.jpg" alt=""></p></div>
                         <div><p>*Nombre de producto*</p></div>
                         <div><p>*Precio*</p></div>
-                        <div><a href="../crear/newProduct.html"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg></a></div>
-                    </td>
-                </tr>
-                <tr>
-                    <td><div><p><img class="imag" src="../imagenes/descarga.jpg" alt=""></p></div>
-                        <div><p>*Nombre de producto*</p></div>
-                        <div><p>*Precio*</p></div>
-                        <div><a href="../crear/newProduct.html"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <div><a href="../crear/newProduct.php"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg></a></div>
                     </td>
                 </tr>
