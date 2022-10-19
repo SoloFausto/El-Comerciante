f4d5e99bd9a6f715d2b76a3c46782215d4be9e6c
Estenux <emiliotagliani81@gmail.com>
2022-10-19 08:58:23 -0300

    joya

---
+++ view/dolcezzainterfaces/tableta/compra/graciasCompra.php
@@ -0,0 +1,34 @@
+<?php Session_Start() ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="../estiloTablet/estiloFinCompra.css">    
+    <title>Tablet</title>
+</head>
+<body>
+    <div class="container-xs">
+        <!-- Este boton te hace volver-->
+        <form action="../helados/helados.php">
+
+        <div class="container ss">
+                <img src="../../resources/dolcezza.png" class="img-thumbnail" alt="Logo de Dolcezza"><br><br>
+        </div>
+        </form>
+        
+        <div class="container text-center">
+                
+                <h2>¡Gracias por elegirnos!</h2><br>
+                <h2>Te redireccionaremos al menu principal</h2>
+            
+        </div>
+
+        <!-- Hacer con php un redireccionamiento automatico luego de 4s -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
