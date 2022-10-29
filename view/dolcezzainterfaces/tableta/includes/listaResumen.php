d365da6ab421f28215f2d900996a6965c21e9409
Estenux <emiliotagliani81@gmail.com>
2022-10-29 09:57:15 -0300

    hola

--- view/dolcezzainterfaces/tableta/includes/listaResumen.php
+++ view/dolcezzainterfaces/tableta/includes/listaResumen.php
@@ -1,15 +1,16 @@
 <div class="row Distancia">
    <!-- Poner la imagen del producto --> <img src="../imagenes/descarga.jpg" class="img-thumbnail" alt="Imagen del producto">
     <div class="col-sm-8">
-        Nombre del Producto <!-- Colocal el nombre del producto -->
+        Helado Triple <!-- Colocal el nombre del producto -->
     </div>
     <div class="col-4">
-        Precio: <?php echo ""; // Aqui aniadir lo que cuesta el producto ?>
+        Precio: <?php echo "$140"; // Aqui aniadir lo que cuesta el producto ?>
     </div>
     <div class="col-sm-4">
         <button type="button" class="btn btn-secondary">
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
         </button>
     </div>
 </div>
--- view/dolcezzainterfaces/tableta/includes/productos.php
+++ view/dolcezzainterfaces/tableta/includes/productos.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-sm-8">col-sm-8</div>
+    <div class="col-sm-8">chocolate</div>
         <div class="col-sm-4">
             <input type="checkbox" class="form-check-input">
         </div>
@@ -7,7 +7,7 @@
 
 
 <div class="row">
-    <div class="col-sm-8">col-sm-8</div>
+    <div class="col-sm-8">Menta</div>
         <div class="col-sm-4">
             <input type="checkbox" class="form-check-input">
         </div>
@@ -16,7 +16,7 @@
 
 
 <div class="row">
-    <div class="col-sm-8">col-sm-8</div>
+    <div class="col-sm-8">Maracuya</div>
         <div class="col-sm-4">
             <input type="checkbox" class="form-check-input">
         </div>
@@ -26,7 +26,7 @@
 
 
 <div class="row">
-    <div class="col-sm-8">col-sm-8</div>
+    <div class="col-sm-8">Dulce de Leche</div>
         <div class="col-sm-4">
             <input type="checkbox" class="form-check-input">
         </div>
