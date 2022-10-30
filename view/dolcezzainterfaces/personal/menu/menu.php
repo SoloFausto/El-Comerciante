16260069b35b0b99c9dfc8096c90f0dda0300bae
Estenux <emiliotagliani81@gmail.com>
2022-10-29 23:01:17 -0300

    Update menu.php

--- view/dolcezzainterfaces/personal/menu/menu.php
+++ view/dolcezzainterfaces/personal/menu/menu.php
@@ -12,7 +12,7 @@ require "/Xampp/htdocs/El-Comerciante/controller/personal/menu/menuController.ph
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-    <title>Menu de el restaurante</title>
+    <title>Menú</title>
 </head>
 
 
